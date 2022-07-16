529510a80996f5f65c4d49545ec3ccf9f22b8501
mfbjave <youremail@domain.com>
2022-07-16 07:11:57 +0000

    Final changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,44 @@
+"""
+Translator.py has two function englishToFrench and FrenchToEnglish
+"""
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def english_to_french(english_text):
+    """
+    english_to_french takes param english_text which is a string
+    """
+    french_text = language_translator.translate(
+    english_text,
+    model_id='en-fr').get_result()
+
+    #i added to my return
+    # so that i would only get the trnaslated sentance and nothing else
+    return french_text['translations'][0]['translation']
+
+def french_to_english(french_text):
+    """
+    french_to_english takes param french_text which is a string
+    """
+    english_text = language_translator.translate(
+    french_text,
+    model_id='fr-en').get_result()
+
+     #i added to my return
+    # so that i would only get the trnaslated sentance and nothing else
+    return english_text['translations'][0]['translation']
